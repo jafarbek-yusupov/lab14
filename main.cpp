5e1c5d8e4c18e9ce2148b723f0144bf028354cb9
j.yusupov <j.yusupov@newuu.uz>
2024-11-25 13:02:46 +0500

    pr2

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 void pr1(){
     int a[3][4];
-    for(int i=0;i<3;i++){    for(int j=0;j<4;j++){    cin >> *(&a[0][0] + i * 4 + j);}}
+    for(int i=0;i<3;i++){    for(int j=0;j<4;j++){    cin >> *(*(a + i) + j);}}
     for(int i=0;i<3;i++){
-        for(int j=0;j<4;j++){    cout << *(&a[0][0] + i * 4 + j) << " ";}
+        for(int j=0;j<4;j++){    cout << *(*(a + i) + j) << " ";}
         cout << endl;
     }
 }
@@ -15,7 +15,13 @@ double probability(int *y,int *w) {
 }
 
 void pr2() {
-    cout << "hello world" << endl;
+    int a[2][3];
+    for(int i=0;i<2;i++){    for(int j=0;j<3;j++){    cin >> *(*(a + i) + j);}}
+    *(*(a + 1) + 2) = 100;
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){    cout << *(*(a + i) + j) << " ";}
+        cout << endl;
+    }
 }
 
 void pr4test() {
@@ -32,6 +38,6 @@ void pr4test() {
 }
 
 int main() {
-    pr4test();
+    pr2();
     return 0;
 }
