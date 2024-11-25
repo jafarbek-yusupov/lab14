5485ef29826e460552eeb139042632eaf4f1240a
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-11-25 12:25:53 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void pr1(){
+    int a[3][4];
+    for(int i=0;i<3;i++){    for(int j=0;j<4;j++){    cin >> *(&a[0][0] + i * 4 + j);}}
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){    cout << *(&a[0][0] + i * 4 + j) << " ";}
+        cout << endl;
+    }
+}
 
+int main() {
+    // pr1();
     return 0;
 }
